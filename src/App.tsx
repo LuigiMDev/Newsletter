4e68e95fbf37c7d361bebd2cad41ef60cb2e5478
Luigi <luigicumaru@gmail.com>
2024-10-13 14:49:12 -0300

    Modificação app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Form from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <div className='bg-gradient-to-r from-sky-500 to-indigo-500 min-h-screen w-full flex flex-col items-center justify-center'>
